86dbc04e52543ed43dd16f505ad1323039396f5c
Jvcn007 <joaovitorvcn07@gmail.com>
2024-10-12 08:20:42 -0300

    Segunda Versão

--- Agenda10/MainPage.xaml.cs
+++ Agenda10/MainPage.xaml.cs
@@ -10,23 +10,25 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+
             try
             {
+                string marca = txt_marca_veiculo.Text;
+                string modelo = txt_modelo_veiculo.Text;
 
                 double etanol = Convert.ToDouble(txt_etanol.Text);
                 double gasolina = Convert.ToDouble(txt_gasolina.Text);
 
                 string msg = "";
 
-                if (etanol <= (gasolina * 0.7)) 
+                if (etanol <= (gasolina * 0.7))
                 {
-                    msg = "O etanol está compensando.";
+                    msg = "O etanol está compensando para o seu " + marca + " " + modelo;
                 }
                 else
                 {
-                    msg = "A gasolina está compensando.";
+                    msg = "A gasolina está compensando para o seu " + marca + " " + modelo;
                 }
-
                 DisplayAlert("Calculado" , msg, "OK");
 
             }
@@ -35,58 +37,12 @@
                 DisplayAlert("Ops", ex.Message, "Fechar");
             }
 
+
+
+
+
+
+
         } // Fecha Método  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*      private void Button_Clicked(object sender, EventArgs e)
-              {
-                {
-                      try
-                      {
-                          double etanol = Convert.ToDouble(txt_etanol.Text);
-                          double gasolina = Convert.ToDouble(txt_gasolina.Text);
-                          string msg = "";
-                          if (etanol <= (gasolina * 0.7))
-                          {
-                              msg = "O etanol está compensando.";
-                          }
-                          else
-                          {
-                              msg = "A gasolina está compensando.";
-                          }
-                          DisplayAlert("Calculado", msg, "OK");
-                      }
-                      catch (Exception ex)
-                      {
-                          DisplayAlert("Ops", ex.Message, "Fechar");
-                      }
-                  }
-              } */
-
-
     } // Fecha Class
 } // Fecha Namespace
